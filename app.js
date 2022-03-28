e60bfe644a2344d4a09cc347c600c80e31a6724b
Jodie Solomons <jodies383@gmail.com>
2022-03-28 12:46:19 +0200

    queue adding and subtracting, counted no of taxis departing

--- app.js
+++ app.js
@@ -1,33 +1,66 @@
-export const TaxiRankApp = () =>{
+export const TaxiRankApp = () => {
     let rank = [{
         destination: "Makhaza",
-        queue: 12
+        queue: 12,
+        departedTaxis: 0
     },
     {
         destination: "Belhar",
-        queue: 34
+        queue: 34,
+        departedTaxis: 0
     },
     {
         destination: "Bellville",
-        queue: 23
+        queue: 23,
+        departedTaxis: 0
     }]
 
     const incrementQueue = (destination) => {
         for (let i = 0; i < rank.length; i++) {
-           if (destination == rank[i].destination){
-           (rank[i].queue++); 
+            if (destination == rank[i].destination) {
+                (rank[i].queue++);
             }
         }
         console.log(rank);
     }
-    const decrementQueue = () => {
-
+    const decrementQueue = (destination) => {
+        for (let i = 0; i < rank.length; i++) {
+            if (destination == rank[i].destination) {
+                if (rank[i].queue > 0) {
+                    (rank[i].queue--);
+                }
+            }
+        }
     }
+    const departTaxi = (destination) => {
+        for (let i = 0; i < rank.length; i++) {
+            if (destination == rank[i].destination) {
+                if (rank[i].queue >= 12) {
+                    (rank[i].queue = 0),
+                    (rank[i].departedTaxis++)
+                }
+                // else {
+                //     return "disabled"
+                // }
+            }
+        }
+    }
+    // const greyedOut = (destination) => {
+    //     for (let i = 0; i < rank.length; i++) {
+    //         if (destination == rank[i].destination) {
+    //             if (rank[i].queue < 12) {
+    //                 return "disabled"
+    //             }
+    //         }
+    //     }
+    // }
     const returnRankInfo = () => rank
 
-    return{
+    return {
         returnRankInfo,
         incrementQueue,
-        decrementQueue
+        decrementQueue,
+        departTaxi,
+        // greyedOut
     }
 }
--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-import {TaxiRankApp} from "./app.js"
+import { TaxiRankApp } from "./app.js"
 
 const taxiRankApp = TaxiRankApp();
 const template = document.querySelector(".taxiTemplate").innerHTML
 const taxiTemplate = Handlebars.compile(template)
 const destinations = document.querySelector(".destinations")
-
+const departTaxi = document.querySelector(".depart")
 let displayDestinations = taxiTemplate({
     rank: taxiRankApp.returnRankInfo()
 });
 destinations.innerHTML = displayDestinations
 
 const increment = document.querySelector(".queueUp")
+const decrement = document.querySelector(".queueDown")
 
 
-increment.addEventListener('click', () => {
-    destinations.innerHTML = ""
-taxiRankApp.incrementQueue(increment.dataset.destination)
+destinations.addEventListener('click', (e) => {
+    console.log(e.target.dataset.destination);
+    if (e.target.name === "plus") {
+        taxiRankApp.incrementQueue(e.target.dataset.destination)
+    } else if (e.target.name === "minus") {
+        taxiRankApp.decrementQueue(e.target.dataset.destination)
+    } else if (e.target.name === "departed") {
+        taxiRankApp.departTaxi(e.target.dataset.destination)
+    }
 
-let displayDestinations = taxiTemplate({
-    rank: taxiRankApp.returnRankInfo()
-});
-
-destinations.innerHTML = displayDestinations
+    let displayDestinations = taxiTemplate({
+        rank: taxiRankApp.returnRankInfo()
+    });
+    destinations.innerHTML = displayDestinations
 
 })
 
